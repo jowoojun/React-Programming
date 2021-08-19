d517df7c4a14f1222278f436f82352f7ddf65119
조우준 <jowoojun77@gmail.com>
2021-08-19 23:07:49 +0900

    css example

---
+++ css-test/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Button from './Button3';
+import Box from './Box4';
+
+function App() {
+  return (
+    <div className="App">
+      <Button size="big" />
+      <Button size="small" />
+      <Box size="big" />
+      <Box size="small" />
+    </div>
+  );
+}
+
+export default App;
